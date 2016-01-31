2638ee2312035d58a8efe0395cb6641cc92a32e3
omda-shareef <omda515@hotmail.co.uk>
2016-01-31 10:23:46 +0300

    EditLawyer Done

--- AbuEhabCourtSystem/AbuEhabCourtSystem/Tables Classes/LawyerCmd.cs
+++ AbuEhabCourtSystem/AbuEhabCourtSystem/Tables Classes/LawyerCmd.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Linq;
 using System.Text;
 
@@ -18,6 +19,19 @@ namespace AbuEhabCourtSystem.Tables_Classes
        public bool EditLawyer(Lowyer law,int lawId)
        {
            law.Id = lawId;
+           var q = CompiledQuery.Compile((DbDataContext dx, int i) => dx.Lowyers.Single(p => p.Id == i));
+           var lawyer = q(DbContext, lawId);
+           lawyer.LowyerName = law.LowyerName;
+           lawyer.Address = law.Address;
+           lawyer.Account = law.Account;
+           lawyer.Mobile = law.Mobile;
+           lawyer.Phone = law.Phone;
+           lawyer.AccountId = law.AccountId;
+           lawyer.FollowUpIssues = law.FollowUpIssues;
+           lawyer.Description = law.Description;
+           lawyer.Status = law.Status;
+
+           DbContext.SubmitChanges();
 
            return true;
        }
